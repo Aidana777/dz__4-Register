6ac239801f2aa1bbd7cc20bc4b08cd3fdfc6cf48
Aidana777 <aidananiazalieva27@gmail.com>
2023-03-26 13:54:11 +0600

    text

--- src/components/Form.js
+++ src/components/Form.js
@@ -23,7 +23,7 @@ const Form = () => {
         <div className=' container'>
             <h1>Register</h1>
             <form className='form' onSubmit={submitHandle}>
-                <input type='name' name='username' placeholder='Username' value={username} onChange={changeHandle} />
+                <input type='text' name='username' placeholder='Username' value={username} onChange={changeHandle} />
                 <input type='email' name='email' placeholder='Email' value={email} onChange={changeHandle} />
                 <input type='password' name='password' value={password} onChange={changeHandle} placeholder='Password' />
                 <input type='password' name='confirmPassword' placeholder='ConfirmPassword' value={confirmPassword} onChange={changeHandle} />
